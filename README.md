2cef6bf452f05cccfe5215368f85104b4830229a
Xer34 <experimental@gmail.com>
2018-12-26 22:17:33 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# unit-4-game
+Coding Bootcamp Crystal Collectors by Alexander Lewis
+
+Word Guess Game is a hangman style word guessing game in which the user attempts to guess the word chosen by the computer. The game is video game themed.
+
+This game is useful to learn the principals of javascript arrays, functions, statements, and loops. It is also useful to begin learning about the power of objects.
+
+This project is not available for additional contribution from the github community, however Alex may be contacted through his website for help or questions regarding the project.
+
+More information:
+
+See the Live Project [Here.](https://xer34.github.io/unit-4-game/)
+
+See Under the Hood [Here.](https://github.com/xer34/unit-4-game)
+
+See Alexander Lewis' [Website](www.alexanderlewis.net) and Entire [Repostitory](https://github.com/xer34?tab=repositories)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,25 @@
 //variables
 
 // random number to win
-var randomNumber = Math.floor((Math.random() * 120) + 19);
+var randomNumber = Math.floor(Math.random() * 120 + 19);
 // each crystal should be 1-12
 var crystalValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  // score var
+// score var
 var score = 0;
 // wins
 var wins = 0;
-// losses 
+// losses
 var losses = 0;
 
 // for loop to create four crystals
 for (var i = 0; i < 4; i++) {
-    var imageCrystal = $("<img>");
-    var randomCrystalVal =  crystalValue[Math.floor(Math.random() * crystalValue.length)];
-    imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "assets/images/godDamnItMarieTheyreMinerals.png");
-    imageCrystal.attr("data-crystalvalue", randomCrystalVal);
-    $("#crystals").append(imageCrystal);
+  var imageCrystal = $("<img>");
+  var randomCrystalVal =
+    crystalValue[Math.floor(Math.random() * crystalValue.length)];
+  imageCrystal.addClass("crystal-image");
+  imageCrystal.attr("src", "assets/images/godDamnItMarieTheyreMinerals.png");
+  imageCrystal.attr("data-crystalvalue", randomCrystalVal);
+  $("#crystals").append(imageCrystal);
 }
 
 //score to match between 19-120
@@ -27,9 +28,8 @@ alert("Match " + randomNumber + " to win!");
 
 //on click event for each crystal
 $(".crystal-image").on("click", function() {
-
-   //set value to THIS value
-  randomCrystalVal = ($(this).attr("data-crystalvalue"));
+  //set value to THIS value
+  randomCrystalVal = $(this).attr("data-crystalvalue");
   // parse it
   randomCrystalVal = parseInt(randomCrystalVal);
   // udpate score
@@ -44,19 +44,19 @@ $(".crystal-image").on("click", function() {
     wins++;
     winsText.textContent = wins;
     score = 0;
-    randomNumber = Math.floor((Math.random() * 120) + 19);
+    randomNumber = Math.floor(Math.random() * 120 + 19);
     $("#randomNumber").text(randomNumber);
-}
+  }
 
-  //losses 
+  //losses
   else if (score >= randomNumber) {
     alert("You died!!");
-    losses++
+    losses++;
     lossesText.textContent = losses;
     score = 0;
-    randomNumber = Math.floor((Math.random() * 120) + 19);
+    randomNumber = Math.floor(Math.random() * 120 + 19);
     $("#randomNumber").text(randomNumber);
-}
+  }
 
-//-----------------------------
+  //-----------------------------
 });
